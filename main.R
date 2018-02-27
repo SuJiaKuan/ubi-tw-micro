b439544c662341d2fc23f6e7b8ffc44bfa0afea3
SuJiaKuan <jiakuan.su@toppano.in>
2018-02-28 01:31:41 +0800

    Add arguments in main script

--- main.R
+++ main.R
@@ -1,3 +1,24 @@
 source("read.R")
 
-families <- ReadFamilyData("./104/inc104.sav")
+kSupportedYears = c(104)
+
+main <- function(year, input.path) {
+    if (!year %in% kSupportedYears) {
+        stop("Year \"", year, "\" is not in supprted list: [",
+             paste(kSupportedYears, collapse = ","), "]")
+    }
+    families <- ReadFamilyData(input.path)
+}
+
+if(!interactive()) {
+    args = commandArgs(trailingOnly = TRUE)
+
+    if (length(args) < 2) {
+        stop("Usage: Rscript main.R year input.path")
+    }
+
+    year = as.integer(args[1])
+    input.path = args[2]
+
+    main(year, input.path)
+}
